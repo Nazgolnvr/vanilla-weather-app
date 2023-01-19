970f7abe673658d27842b7f3346d49e6b26b7d05
Nazgolnvr <nazgolnvr93@gmail.com>
2023-01-19 17:05:42 +0330

    showing current status

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
 function displayTemperature(response) {
-  console.log(response.data.main.temp);
+  let tempeartureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  tempeartureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = " aab327076aaf3ac1082bb1368b63d1da";
